50d6b66dc5d79e9a954d43a86c7d57c801a977fd
Cody  Carr <codycarr@infra04-wg012.eduroam.wireless.umn.edu>
2023-02-28 18:25:00 -0600

    fixed link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys, os
+sys.path.insert(0, os.path.abspath('../../'))
+
 project = 'SALT Model'
 copyright = '2023, Cody Carr'
 author = 'Cody Carr'
